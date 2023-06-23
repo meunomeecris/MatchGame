01c12eabdc1d810f7cd956a4db976daef9ca8b83
Cris Messias <meunomeecriss@gmail.com>
2023-06-23 16:48:51 +0200

    Initial Commit

---
+++ MachGame/MachGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MachGameApp.swift
+//  MachGame
+//
+//  Created by Cris Messias on 23/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MachGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
