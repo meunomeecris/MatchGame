52e501450096851cc9fcaa7ac3cc86d492c4c2d9
Cris Messias <meunomeecriss@gmail.com>
2023-06-28 17:23:42 +0200

    👩🏻‍💻 - Building Model and ViewModel
    Sorting out the logic and connect to ViewModel

--- MatchGame/EmojiMatchGame.swift
+++ MatchGame/EmojiMatchGame.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class EmojiMatchGame {
+    static let emojis: [String] = ["🇧🇷", "🏳️‍🌈", "🇹🇳", "🇫🇷", "🇨🇦", "🇯🇲", "🇧🇩", "🇪🇨", "🇬🇹", "🇵🇼", "🇺🇾", "🇱🇨", "🇧🇫", "🇷🇼", "🇪🇺", "🇯🇵"]
+    
+    static func createMatchGame() -> MatchGame<String> {
+        MatchGame<String>(numberOfPairsOfCards: 4) { parIndex in
+            emojis[parIndex]
+        }
+    }
+    
+    private var model: MatchGame<String> = createMatchGame()
+    
+    var cards: Array<MatchGame<String>.Card> {
+        return model.cards
+    }
+}
