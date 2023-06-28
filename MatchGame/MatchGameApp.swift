ddbae8295b0f1586461b517aacf06f976f3d88a1
Cris Messias <meunomeecriss@gmail.com>
2023-06-28 15:07:41 +0200

    🪲 renaming bug

---
+++ MatchGame/MatchGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchGame.swift
+//  MatchGame
+//
+//  Created by Cris Messias on 16/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MatchGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
